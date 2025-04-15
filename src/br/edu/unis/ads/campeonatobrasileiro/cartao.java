ce18a8a5dd656987dd4e299b116122e894b0d984
Thiago1107-bi <tgones160@gmail.com>
2025-04-15 01:03:20 -0300

    Update cartao.java

--- src/br/edu/unis/ads/campeonatobrasileiro/cartao.java
+++ src/br/edu/unis/ads/campeonatobrasileiro/cartao.java
@@ -1,9 +1,51 @@
-class Cartao{
-    String cor;
-    String participante;
-    String jogo;
+Class Cartao {
 
-    Cartao(String args[]){
+    // Atributos 
+    Private String cor;
+    Private String participante;
+    Private String jogo;
+    Private String funcaoDocartao;
 
+   
+   // Construtor 
+    public Cartao(String args[]){
+        this.cor = cor;
+        this.participante = participante;
+        this.jogo = jogo;
+        this.funçaodocartao = funçaoDocartao;
+        
+        }
+
+    // Getters
+    public String getcor() {
+        retorn cor;
     }
+
+    public String getparticipante() {
+        retorn participante;
+    }
+
+    public String getjogo() {
+        retorn jogo;
+    }
+
+    public String getfuncaoDocartao() {
+        retorn funcaoDocartao;
+    }
+
+    // Setters
+     public void setCor(String cor) {
+        this.cor = cor;
+     }
+
+     public void setparticipante(String participante) {
+        this.participante = participante;
+     }
+
+     public void setjogo(String jogo) {
+        this.jogo = jogo;
+     }
+     public void setfuncaoDocartao(String funcaoDocartao) {
+        this.funcaoDocartao = funcaoDocartao;
+     }
 }
